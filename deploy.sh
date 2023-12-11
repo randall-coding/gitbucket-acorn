f134953e47eb68c8680ae7211d8d598797594aba
Randall Coding <randallcoding@protonmail.com>
2023-12-11 17:06:44 +0700

    Updates container RAM to fix crash issue

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 acorn build -t ghcr.io/randall-coding/acorn/gitbucket && \
 acorn push ghcr.io/randall-coding/acorn/gitbucket && \
-acorn run -n gitbucket ghcr.io/randall-coding/acorn/gitbucket
+acorn run -n gitbucket --memory=1Gi ghcr.io/randall-coding/acorn/gitbucket
